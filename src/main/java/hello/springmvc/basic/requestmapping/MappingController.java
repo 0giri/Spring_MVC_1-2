daf2b45a771fac90e37e81ab9f9720cbbab77a93
0giri <higiri@naver.com>
2021-12-28 03:04:09 +0900

    Initial commit ~ Http message converter

---
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,20 @@
+package hello.springmvc.basic.requestmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MappingController {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @GetMapping(value = "/hello-basic" , produces = MediaType.APPLICATION_JSON_VALUE)
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+}
